226946cacc9540de27b71df2128734d45440c2f9
darraghdarragh <darragh_morrissey@hotmail.com>
2025-07-18 18:28:38 +0100

    Merge remote changes and add README

--- darraghops_backend/settings.py
+++ darraghops_backend/settings.py
@@ -136,3 +136,4 @@ AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_FILE_UPLOAD_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
